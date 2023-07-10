c18bef309d34c337e5f30d1e81e1d68b418e338d
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-10 07:42:03 -0300

    feat: implementation search by id of garden

--- server/src/main/routes/garden-router.ts
+++ server/src/main/routes/garden-router.ts
@@ -2,11 +2,25 @@ import { prisma } from "@/external/repositories/postgres/helpers"
 import { Router } from "express"
 
 export default (router: Router) => {
-  router.get("/garden", async (req, res) => {
+  router.get("/gardens", async (req, res) => {
     const garden = await prisma.garden.findMany()
 
     return garden
       ? res.status(200).json({ garden })
       : res.status(400).json({ message: "No record found for garden." })
+  })
+
+  router.get("/gardens/:id_garden/garden", async (req, res) => {
+    const { id_garden } = req.params
+
+    const garden = await prisma.garden.findFirst({
+      where: {
+        id_garden,
+      },
+    })
+
+    return garden
+      ? res.status(200).json({ garden })
+      : res.status(400).json({ message: "Garden not found." })
   })
 }
