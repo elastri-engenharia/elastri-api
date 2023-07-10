8817afa1290555b96aa958b53ed607f80bc88cc8
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-10 09:35:36 -0300

    feat: implementation search by ID of  measurement

--- server/src/main/routes/measurement-router.ts
+++ server/src/main/routes/measurement-router.ts
@@ -9,4 +9,20 @@ export default (router: Router) => {
       ? res.status(200).json({ measurements })
       : res.status(400).json({ message: "No record  found for measurement." })
   })
+
+  router.get("/measurements/:id_measurement/measurement", async (req, res) => {
+    const { id_measurement } = req.params
+
+    const measurement = await prisma.measurement.findFirst({
+      where: {
+        id_measurement,
+      },
+    })
+
+    return measurement
+      ? res.status(200).json({
+          measurement,
+        })
+      : res.status(400).json({ message: "Measured not found." })
+  })
 }
