7be6c71d31df47b855d36778bd95a24d75a28bcb
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-10 08:35:30 -0300

    feat: implementation created subfields

--- server/src/main/routes/subfield-router.ts
+++ server/src/main/routes/subfield-router.ts
@@ -23,4 +23,27 @@ export default (router: Router) => {
       ? res.status(200).json({ subfield })
       : res.status(400).json({ message: "SubField not found." })
   })
+
+  router.post("/subfields/create", async (req, res) => {
+    const { name } = req.body
+
+    const subfieldOrNotFound = await prisma.subField.findFirst({
+      where: {
+        name,
+      },
+    })
+
+    if (subfieldOrNotFound)
+      return res.status(400).json({ message: "Subfield already exists." })
+
+    const subfield = await prisma.subField.create({
+      data: {
+        name,
+      },
+    })
+
+    return subfield
+      ? res.status(201).json({ subfield })
+      : res.status(400).json({ message: "Unable to register subfield." })
+  })
 }
