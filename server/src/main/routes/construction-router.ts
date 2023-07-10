fa7c77b5f164db9d96974e7f6308700fbad4d5d4
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-10 14:50:08 -0300

    feat: search by ID in construction

--- server/src/main/routes/construction-router.ts
+++ server/src/main/routes/construction-router.ts
@@ -9,4 +9,21 @@ export default (router: Router) => {
       ? res.status(200).json({ constructions })
       : res.status(400).json({ message: "No record found for constructions." })
   })
+
+  router.get(
+    "/constructions/:id_construction/construction",
+    async (req, res) => {
+      const { id_construction } = req.params
+
+      const construction = await prisma.construction.findFirst({
+        where: {
+          id_construction,
+        },
+      })
+
+      return construction
+        ? res.status(200).json({ construction })
+        : res.status(400).json({ message: "Construction not found." })
+    },
+  )
 }
