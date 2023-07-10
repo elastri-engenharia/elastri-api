07bf49c835a23f5aed55199f4b9d302c3b35046c
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-10 08:10:03 -0300

    feat: implementation search by id of subfield

--- server/src/main/routes/subfield-router.ts
+++ server/src/main/routes/subfield-router.ts
@@ -9,4 +9,18 @@ export default (router: Router) => {
       ? res.status(200).json({ subfields })
       : res.status(400).json({ message: "No record found for garnde." })
   })
+
+  router.get("/subfields/:id_subField/subfield", async (req, res) => {
+    const { id_subField } = req.params
+
+    const subfield = await prisma.subField.findFirst({
+      where: {
+        id_subField,
+      },
+    })
+
+    return subfield
+      ? res.status(200).json({ subfield })
+      : res.status(400).json({ message: "SubField not found." })
+  })
 }
