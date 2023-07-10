8547051df96ae9d0e5444eec964fe6c7f3c44720
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-10 16:34:25 -0300

    feat: implementation search by ID to third

--- server/src/main/routes/third-router.ts
+++ server/src/main/routes/third-router.ts
@@ -9,4 +9,18 @@ export default (router: Router) => {
       ? res.status(200).json({ thirds })
       : res.status(400).json({ message: "No record found third." })
   })
+
+  router.get("/thirds/:id_third/third", async (req, res) => {
+    const { id_third } = req.params
+
+    const third = await prisma.third.findFirst({
+      where: {
+        id_third,
+      },
+    })
+
+    return third
+      ? res.status(200).json({ third })
+      : res.status(400).json({ message: "Third nor found." })
+  })
 }
