cb5b04b0421b24867cc46b871ecdeb4cebaa6520
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-10 11:28:38 -0300

    feat: implementation search by id to company

--- server/src/main/routes/company-router.ts
+++ server/src/main/routes/company-router.ts
@@ -11,4 +11,18 @@ export default (router: Router) => {
           message: "No record found for company.",
         })
   })
+
+  router.get("/companys/:id_company/company", async (req, res) => {
+    const { id_company } = req.params
+
+    const company = await prisma.company.findFirst({
+      where: {
+        id_company,
+      },
+    })
+
+    return company
+      ? res.status(200).json({ company })
+      : res.status(400).json({ message: "Company not found." })
+  })
 }
