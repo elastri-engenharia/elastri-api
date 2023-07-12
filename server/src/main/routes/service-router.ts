9bb83124fdcdef4957cac127e1381c971a96723c
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-12 07:32:39 -0300

    feat: implementation search by id to service

--- server/src/main/routes/service-router.ts
+++ server/src/main/routes/service-router.ts
@@ -9,4 +9,18 @@ export default (router: Router) => {
       ? res.status(200).json({ services })
       : res.status(400).json({ message: "No record found for services." })
   })
+
+  router.get("/services/:id_service/service", async (req, res) => {
+    const { id_service } = req.params
+
+    const service = await prisma.service.findFirst({
+      where: {
+        id_service,
+      },
+    })
+
+    return service
+      ? res.status(200).json({ service })
+      : res.status(404).json({ message: "Service not found." })
+  })
 }
