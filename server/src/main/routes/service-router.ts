2ad99d6e4a13a0940be879d7ecf89029cc23c175
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-12 09:09:12 -0300

    feat: implementation search by id to service added select fields

--- server/src/main/routes/service-router.ts
+++ server/src/main/routes/service-router.ts
@@ -3,7 +3,42 @@ import { Router } from "express"
 
 export default (router: Router) => {
   router.get("/services", async (req, res) => {
-    const services = await prisma.service.findMany()
+    const services = await prisma.service.findMany({
+      select: {
+        id_service: true,
+        code_service: true,
+        name_service: true,
+        code_totvs: true,
+        activity: true,
+        garden: {
+          select: {
+            name: true,
+          },
+        },
+        subfield: {
+          select: {
+            id_subField: true,
+            name: true,
+          },
+        },
+        undMeasure: {
+          select: {
+            id_measurement: true,
+            symbol: true,
+          },
+        },
+        foreseen: true,
+        advance: true,
+        construction_idConstruction: {
+          select: {
+            id_construction: true,
+            code_construction: true,
+            name_construction: true,
+          },
+        },
+        disabled_service: true,
+      },
+    })
 
     return services
       ? res.status(200).json({ services })
@@ -17,10 +52,44 @@ export default (router: Router) => {
       where: {
         id_service,
       },
+      select: {
+        id_service: true,
+        code_service: true,
+        name_service: true,
+        code_totvs: true,
+        activity: true,
+        garden: {
+          select: {
+            name: true,
+          },
+        },
+        subfield: {
+          select: {
+            id_subField: true,
+            name: true,
+          },
+        },
+        undMeasure: {
+          select: {
+            id_measurement: true,
+            symbol: true,
+          },
+        },
+        foreseen: true,
+        advance: true,
+        construction_idConstruction: {
+          select: {
+            id_construction: true,
+            code_construction: true,
+            name_construction: true,
+          },
+        },
+        disabled_service: true,
+      },
     })
 
     return service
       ? res.status(200).json({ service })
-      : res.status(404).json({ message: "Service not found." })
+      : res.status(400).json({ message: "Service not found." })
   })
 }
