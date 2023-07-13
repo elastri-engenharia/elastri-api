e4907f671226cf655a38a45bcdb29625991aa2e4
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-13 07:36:28 -0300

    feat: implementation create new service

--- server/src/main/routes/service-router.ts
+++ server/src/main/routes/service-router.ts
@@ -36,6 +36,13 @@ export default (router: Router) => {
             name_construction: true,
           },
         },
+        collaborator_idCollaborator: {
+          select: {
+            id_collaborator: true,
+            matriculation: true,
+            name_collaborator: true,
+          },
+        },
         disabled_service: true,
       },
     })
@@ -84,6 +91,13 @@ export default (router: Router) => {
             name_construction: true,
           },
         },
+        collaborator_idCollaborator: {
+          select: {
+            id_collaborator: true,
+            matriculation: true,
+            name_collaborator: true,
+          },
+        },
         disabled_service: true,
       },
     })
@@ -91,5 +105,112 @@ export default (router: Router) => {
     return service
       ? res.status(200).json({ service })
       : res.status(400).json({ message: "Service not found." })
+  })
+
+  router.post("/services/create", async (req, res) => {
+    const {
+      code_service,
+      name_service,
+      code_totvs,
+      activity,
+      gardenId_garden,
+      subFieldId_subField,
+      measurementId_measurement,
+      foreseen,
+      advance,
+      constructionId_construction,
+      collaborator_idCollaborator,
+      disabled_service,
+    } = req.body
+
+    const verifySubField = subFieldId_subField ? subFieldId_subField : undefined
+
+    const serviceOrNotFound = await prisma.service.findFirst({
+      where: {
+        code_service,
+      },
+    })
+
+    if (serviceOrNotFound)
+      return res.status(400).json({ message: "Service already exists." })
+
+    if (typeof verifySubField === "undefined") {
+      const service = await prisma.service.create({
+        data: {
+          code_service,
+          name_service,
+          code_totvs,
+          activity,
+          garden: {
+            connect: {
+              id_garden: gardenId_garden,
+            },
+          },
+          undMeasure: {
+            connect: {
+              id_measurement: measurementId_measurement,
+            },
+          },
+          foreseen,
+          advance,
+          disabled_service,
+          collaborator_idCollaborator: {
+            connect: {
+              id_collaborator: collaborator_idCollaborator,
+            },
+          },
+          construction_idConstruction: {
+            connect: {
+              id_construction: constructionId_construction,
+            },
+          },
+        },
+      })
+
+      return service
+        ? res.status(201).json({ service })
+        : res.status(400).json({ message: "Unable to register service." })
+    }
+
+    const createdService = await prisma.service.create({
+      data: {
+        code_service,
+        name_service,
+        code_totvs,
+        activity,
+        garden: {
+          connect: {
+            id_garden: gardenId_garden,
+          },
+        },
+        subfield: {
+          connect: {
+            id_subField: subFieldId_subField,
+          },
+        },
+        undMeasure: {
+          connect: {
+            id_measurement: measurementId_measurement,
+          },
+        },
+        foreseen,
+        advance,
+        disabled_service,
+        collaborator_idCollaborator: {
+          connect: {
+            id_collaborator: collaborator_idCollaborator,
+          },
+        },
+        construction_idConstruction: {
+          connect: {
+            id_construction: constructionId_construction,
+          },
+        },
+      },
+    })
+
+    return createdService
+      ? res.status(201).json({ createdService })
+      : res.status(400).json({ message: "Unable to register service." })
   })
 }
