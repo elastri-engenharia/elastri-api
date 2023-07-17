c780102afd4ab5e0433a66dd9f8142d12cbbbdc3
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-17 11:18:17 -0300

    chore: installing and setup axios, react query

--- server/src/main/config/app.ts
+++ server/src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express from "express"
+import cors from "cors"
 import setupRouter from "./setup-router"
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 setupRouter(app)
 
 export { app }
