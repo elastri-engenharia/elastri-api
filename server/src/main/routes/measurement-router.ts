f0d8da4d9967a5b8281cf621136566bd22da448f
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-17 15:50:11 -0300

    feat: defining access permission to measurement routes

--- server/src/main/routes/measurement-router.ts
+++ server/src/main/routes/measurement-router.ts
@@ -1,86 +1,111 @@
 import { prisma } from "@/external/repositories/postgres/helpers"
 import { Router } from "express"
 
+import can from "@/presentation/middleware/acl"
+import auth from "@/presentation/middleware/authentication"
+
 export default (router: Router) => {
-  router.get("/measurements", async (req, res) => {
-    const measurements = await prisma.measurement.findMany()
+  router.get(
+    "/measurements",
+    auth,
+    can(["ADMIN", "ACCESS_ADMIN", "ACCESS_ADMIN_RDC"]),
+    async (req, res) => {
+      const measurements = await prisma.measurement.findMany()
 
-    return measurements
-      ? res.status(200).json({ measurements })
-      : res.status(400).json({ message: "No record  found for measurement." })
-  })
+      return measurements
+        ? res.status(200).json({ measurements })
+        : res.status(400).json({ message: "No record  found for measurement." })
+    },
+  )
 
-  router.get("/measurements/:id_measurement/measurement", async (req, res) => {
-    const { id_measurement } = req.params
+  router.get(
+    "/measurements/:id_measurement/measurement",
+    auth,
+    can(["ADMIN", "ACCESS_ADMIN", "ACCESS_ADMIN_RDC"]),
+    async (req, res) => {
+      const { id_measurement } = req.params
 
-    const measurement = await prisma.measurement.findFirst({
-      where: {
-        id_measurement,
-      },
-    })
-
-    return measurement
-      ? res.status(200).json({
-          measurement,
-        })
-      : res.status(400).json({ message: "Measured not found." })
-  })
-
-  router.post("/measurements/create", async (req, res) => {
-    const { name_measurement, symbol } = req.body
-
-    const measurementOrNotFound = await prisma.measurement.findFirst({
-      where: {
-        symbol,
-      },
-    })
-
-    if (measurementOrNotFound)
-      return res.status(400).json({ message: "Sorry symbol is in use." })
-
-    const measurement = await prisma.measurement.create({
-      data: {
-        name_measurement,
-        symbol,
-      },
-    })
-
-    return measurement
-      ? res.status(201).json({ measurement })
-      : res.status(400).json({ message: "Unable to register measurement." })
-  })
-
-  router.patch("/measurements/:id_measurement/update", async (req, res) => {
-    const { id_measurement } = req.params
-
-    const { name_measurement, symbol } = req.body
-
-    const measurementIsInNotUse = await prisma.measurement.findFirst({
-      where: {
-        NOT: {
+      const measurement = await prisma.measurement.findFirst({
+        where: {
           id_measurement,
         },
-        AND: {
+      })
+
+      return measurement
+        ? res.status(200).json({
+            measurement,
+          })
+        : res.status(400).json({ message: "Measured not found." })
+    },
+  )
+
+  router.post(
+    "/measurements/create",
+    auth,
+    can(["ADMIN", "ACCESS_ADMIN", "ACCESS_ADMIN_RDC"]),
+    async (req, res) => {
+      const { name_measurement, symbol } = req.body
+
+      const measurementOrNotFound = await prisma.measurement.findFirst({
+        where: {
           symbol,
         },
-      },
-    })
+      })
 
-    if (measurementIsInNotUse)
-      return res.status(400).json({ message: "Sorry symbol is in use." })
+      if (measurementOrNotFound)
+        return res.status(400).json({ message: "Sorry symbol is in use." })
 
-    const measurement = await prisma.measurement.update({
-      where: {
-        id_measurement,
-      },
-      data: {
-        name_measurement,
-        symbol,
-      },
-    })
+      const measurement = await prisma.measurement.create({
+        data: {
+          name_measurement,
+          symbol,
+        },
+      })
 
-    return measurement
-      ? res.status(200).json({ measurement })
-      : res.status(400).json({ message: "Unable to update this measurement." })
-  })
+      return measurement
+        ? res.status(201).json({ measurement })
+        : res.status(400).json({ message: "Unable to register measurement." })
+    },
+  )
+
+  router.patch(
+    "/measurements/:id_measurement/update",
+    auth,
+    can(["ADMIN", "ACCESS_ADMIN", "ACCESS_ADMIN_RDC"]),
+    async (req, res) => {
+      const { id_measurement } = req.params
+
+      const { name_measurement, symbol } = req.body
+
+      const measurementIsInNotUse = await prisma.measurement.findFirst({
+        where: {
+          NOT: {
+            id_measurement,
+          },
+          AND: {
+            symbol,
+          },
+        },
+      })
+
+      if (measurementIsInNotUse)
+        return res.status(400).json({ message: "Sorry symbol is in use." })
+
+      const measurement = await prisma.measurement.update({
+        where: {
+          id_measurement,
+        },
+        data: {
+          name_measurement,
+          symbol,
+        },
+      })
+
+      return measurement
+        ? res.status(200).json({ measurement })
+        : res
+            .status(400)
+            .json({ message: "Unable to update this measurement." })
+    },
+  )
 }
