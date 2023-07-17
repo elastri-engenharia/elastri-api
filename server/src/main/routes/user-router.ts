2afedb83a2dc5dd74da314577d2d834ef0ede149
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-17 15:14:47 -0300

    feat: defining access permission to user routes

--- server/src/main/routes/user-router.ts
+++ server/src/main/routes/user-router.ts
@@ -4,106 +4,152 @@ import { prisma } from "@/external/repositories/postgres/helpers"
 import { Role } from "@prisma/client"
 import { encoder } from "@/external/encoder/bcrypt-encoder"
 
+import auth from "@/presentation/middleware/authentication"
+import can from "@/presentation/middleware/acl"
+
 export default (router: Router) => {
-  router.get("/users/roles", async (req, res) => {
+  router.get("/users/roles", auth, can(["ADMIN"]), async (req, res) => {
     const roles = Role
 
     return res.status(200).json(roles)
   })
 
-  router.get("/users", async (req, res) => {
-    const users = await prisma.user.findMany()
+  router.get(
+    "/users",
+    auth,
+    can(["ADMIN", "ACCESS_ADMIN", "ACCESS_ADMIN_RDC", "ACCESS_ADMIN_SNACK"]),
+    async (req, res) => {
+      const users = await prisma.user.findMany()
 
-    return res.status(200).json(users)
-  })
+      return res.status(200).json(users)
+    },
+  )
 
-  router.post("/users/create", async (req, res) => {
-    const { username, email, password, role } = req.body
+  router.post(
+    "/users/create",
+    auth,
+    can([
+      "ADMIN",
+      "ACCESS_ADMIN",
+      "ACCESS_ADMIN_RDC",
+      "ACCESS_ADMIN_SNACK",
+      "ACCESS_FUNC_RDC",
+      "ACCESS_FUNC_SNACK",
+    ]),
+    async (req, res) => {
+      const { username, email, password, role } = req.body
 
-    const userExists = await prisma.user.findFirst({
-      where: {
-        OR: [{ email: email }, { username: username }],
-      },
-    })
-
-    if (userExists) {
-      return res
-        .status(400)
-        .json({ message: "Username or Email already exists" })
-    }
-
-    const passwordEncrypted = await encoder(password)
-
-    const users = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password: passwordEncrypted,
-        role,
-      },
-    })
-
-    return res.status(201).json({ users })
-  })
-
-  router.get("/users/:id_user/user", async (req, res) => {
-    const { id_user } = req.params
-
-    const user = await prisma.user.findFirst({
-      where: {
-        id_user,
-      },
-    })
-
-    return user
-      ? res.status(200).json({ user })
-      : res.status(400).json({ message: "User not found" })
-  })
-
-  router.put("/users/:id_user/update", async (req, res) => {
-    const { id_user } = req.params
-    const { username, email, password, role } = req.body
-
-    const usernameOrNotFound = await prisma.user.findFirst({
-      where: {
-        id_user: {
-          not: id_user,
+      const userExists = await prisma.user.findFirst({
+        where: {
+          OR: [{ email: email }, { username: username }],
         },
-        AND: { username },
-      },
-    })
+      })
 
-    const emailOrNotFound = await prisma.user.findFirst({
-      where: {
-        id_user: {
-          not: id_user,
+      if (userExists) {
+        return res
+          .status(400)
+          .json({ message: "Username or Email already exists" })
+      }
+
+      const passwordEncrypted = await encoder(password)
+
+      const users = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: passwordEncrypted,
+          role,
         },
-        AND: { email },
-      },
-    })
+      })
 
-    if (usernameOrNotFound) {
-      return res.status(400).json({ message: "Sorry username is in use." })
-    }
+      return res.status(201).json({ users })
+    },
+  )
 
-    if (emailOrNotFound) {
-      return res.status(400).json({ message: "Sorry email is in email." })
-    }
+  router.get(
+    "/users/:id_user/user",
+    auth,
+    can([
+      "ADMIN",
+      "ACCESS_ADMIN",
+      "ACCESS_ADMIN_RDC",
+      "ACCESS_ADMIN_SNACK",
+      "ACCESS_FUNC_RDC",
+      "ACCESS_FUNC_SNACK",
+      "ACCESS_VISITOR_SNACK",
+    ]),
+    async (req, res) => {
+      const { id_user } = req.params
 
-    const userUpdated = await prisma.user.update({
-      where: {
-        id_user,
-      },
-      data: {
-        username,
-        email,
-        password,
-        role,
-      },
-    })
+      const user = await prisma.user.findFirst({
+        where: {
+          id_user,
+        },
+      })
 
-    return userUpdated
-      ? res.status(200).json({ userUpdated })
-      : res.status(400).json({ message: "Unable to update this user." })
-  })
+      return user
+        ? res.status(200).json({ user })
+        : res.status(400).json({ message: "User not found" })
+    },
+  )
+
+  router.put(
+    "/users/:id_user/update",
+    auth,
+    can([
+      "ADMIN",
+      "ACCESS_ADMIN",
+      "ACCESS_ADMIN_RDC",
+      "ACCESS_ADMIN_SNACK",
+      "ACCESS_FUNC_RDC",
+      "ACCESS_FUNC_SNACK",
+      "ACCESS_VISITOR_SNACK",
+    ]),
+    async (req, res) => {
+      const { id_user } = req.params
+      const { username, email, password, role } = req.body
+
+      const usernameOrNotFound = await prisma.user.findFirst({
+        where: {
+          id_user: {
+            not: id_user,
+          },
+          AND: { username },
+        },
+      })
+
+      const emailOrNotFound = await prisma.user.findFirst({
+        where: {
+          id_user: {
+            not: id_user,
+          },
+          AND: { email },
+        },
+      })
+
+      if (usernameOrNotFound) {
+        return res.status(400).json({ message: "Sorry username is in use." })
+      }
+
+      if (emailOrNotFound) {
+        return res.status(400).json({ message: "Sorry email is in email." })
+      }
+
+      const userUpdated = await prisma.user.update({
+        where: {
+          id_user,
+        },
+        data: {
+          username,
+          email,
+          password,
+          role,
+        },
+      })
+
+      return userUpdated
+        ? res.status(200).json({ userUpdated })
+        : res.status(400).json({ message: "Unable to update this user." })
+    },
+  )
 }
