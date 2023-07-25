99fb19f97cf34b783e7a0737b1b6e3bae034f401
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-25 09:32:14 -0300

    chore: creating entities collaborator and construction

--- web/src/entity/Collaborator.ts
+++ web/src/entity/Collaborator.ts
@@ -1,7 +1,30 @@
+import { z } from "zod";
+
 export interface Collaborator {
+  id_collaborator: string;
   matriculation: string;
   name_collaborator: string;
   office_collaborator: string;
   disabled_collaborator: boolean;
   responsible: boolean;
 }
+
+const regex = new RegExp(/^\d{1,10}$/);
+
+export const collaboratorFormSchema = z.object({
+  matriculation: z.string().regex(regex, "Este campo deve ser numérico."),
+  name_collaborator: z
+    .string()
+    .nonempty("Este campo é obrigatório.")
+    .max(100, "Deve conter no máximo 100 caracteres.")
+    .toUpperCase(),
+  office_collaborator: z
+    .string()
+    .nonempty("Este campo é obrigatório.")
+    .toUpperCase(),
+  disabled_collaborator: z.boolean(),
+  responsible: z.boolean(),
+  construction_idConstruction: z.string(),
+});
+
+export type collaboratorFormData = z.infer<typeof collaboratorFormSchema>;
---
+++ web/src/entity/Construction.ts
@@ -0,0 +1,7 @@
+export interface Construction {
+  id_construction: string;
+  code_construction: string;
+  name_construction: string;
+  company_idCompany: string;
+  user_idUser: string;
+}
