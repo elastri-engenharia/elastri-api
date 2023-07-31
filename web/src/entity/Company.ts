b7919a067d8ef813b2dede6680cd41a4a60e0506
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-07-31 09:37:39 -0300

    feat: declaring behavior of the enterprise entity

--- web/src/entity/Company.ts
+++ web/src/entity/Company.ts
@@ -3,10 +3,16 @@ import { z } from "zod";
 export interface Company {
   id_company: string;
   company_name: string;
+  users?: [
+    {
+      username: string;
+    }
+  ];
 }
 
 export const companyFormSchema = z.object({
-  company_name: z.string().nonempty("Este campo é obrigatório."),
+  company_name: z.string().toUpperCase().nonempty("Este campo é obrigatório."),
+  users: z.array(z.string()).optional(),
 });
 
 export type companyFormData = z.infer<typeof companyFormSchema>;
