856e09addf3ad565954b0b41a94fd8cdfd3b8fad
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-08-08 13:02:21 -0300

    feat: added twMerge

--- web/src/components/FormElements/FormElementsContainerDivider.tsx
+++ web/src/components/FormElements/FormElementsContainerDivider.tsx
@@ -1,11 +1,15 @@
-import { ReactNode } from "react";
+import { HTMLAttributes, ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
-interface FormElementsContainerDivider {
+interface FormElementsContainerDivider extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
 }
 
 export default function FormElementsContainerDivider({
   children,
+  ...rest
 }: FormElementsContainerDivider) {
-  return <div className="w-full xl:w-1/2">{children}</div>;
+  return (
+    <div className={twMerge("w-full xl:w-1/2", rest.className)}>{children}</div>
+  );
 }
