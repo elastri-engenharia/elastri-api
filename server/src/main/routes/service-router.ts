cce1e8bbcc77f6451bdc178427a37780ad244a24
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-09-06 07:57:11 -0300

    chore: functionality was created to update services

--- server/src/main/routes/service-router.ts
+++ server/src/main/routes/service-router.ts
@@ -212,4 +212,109 @@ export default (router: Router) => {
         : res.status(400).json({ message: "Unable to register service." })
     },
   )
+
+  router.put(
+    "/services/:id_service/update",
+    auth,
+    can(["ADMIN", "ACCESS_ADMIN", "ACCESS_ADMIN_RDC"]),
+    async (req, res) => {
+      const { id_service } = req.params
+
+      const {
+        code_service,
+        name_service,
+        code_totvs,
+        activity,
+        gardenId_garden,
+        subFieldId_subField,
+        measurementId_measurement,
+        areaId_area,
+        foreseen,
+        advance,
+        constructionId_construction,
+        collaborator_idCollaborator,
+        disabled_service,
+      } = req.body
+
+      const data: {
+        code_service: string
+        name_service: string
+        code_totvs: string
+        activity: string
+        garden: { connect: { id_garden: string } }
+        subfield?: { connect: { id_subField: string } }
+        undMeasure: { connect: { id_measurement: string } }
+        Area?: { connect: { id_area: string } }
+        foreseen: string
+        advance: string
+        construction_idConstruction: { connect: { id_construction: string } }
+        collaborator_idCollaborator?: {
+          set?: []
+          connect?: [
+            {
+              id_collaborator: string
+            },
+          ]
+        }
+        disabled_service: boolean
+      } = {
+        code_service,
+        name_service,
+        code_totvs,
+        activity,
+        garden: { connect: { id_garden: gardenId_garden } },
+        undMeasure: { connect: { id_measurement: measurementId_measurement } },
+        foreseen,
+        advance,
+        construction_idConstruction: {
+          connect: { id_construction: constructionId_construction },
+        },
+        disabled_service,
+      }
+
+      const serviceIsInNotUse = await prisma.service.findFirst({
+        where: {
+          NOT: {
+            id_service,
+          },
+          AND: {
+            code_service,
+          },
+        },
+      })
+
+      if (serviceIsInNotUse)
+        return res.status(400).json({ message: "Sorry service is in use." })
+
+      if (subFieldId_subField?.length) {
+        data.subfield = { connect: { id_subField: subFieldId_subField } }
+      }
+
+      if (areaId_area?.length) {
+        data.Area = { connect: { id_area: areaId_area } }
+      }
+
+      if (collaborator_idCollaborator?.length) {
+        data.collaborator_idCollaborator = {
+          set: [],
+          connect: collaborator_idCollaborator,
+        }
+      } else {
+        data.collaborator_idCollaborator = {
+          set: [],
+        }
+      }
+
+      const serviceUpdated = await prisma.service.update({
+        where: {
+          id_service,
+        },
+        data,
+      })
+
+      return serviceUpdated
+        ? res.status(200).json({ serviceUpdated })
+        : res.status(400).json({ message: "Unable to register service." })
+    },
+  )
 }
